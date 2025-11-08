6d6947a6def429941b9a81e7fdaf66a70272c27b
Naveen Kumar <knaveenk869@gmail.com>
2025-11-08 13:42:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const container = document.querySelector('.container')
+for(let i=0;i<800;i++){
+	const box = document.createElement('div');
+	box.classList.add('box')
+	
+	container.append(box);
+	box.addEventListener('mouseover',()=>{
+		box.style.backgroundColor = 'blue'
+	})
+	box.addEventListener('mouseout',()=>{
+		setTimeout(()=>{
+			box.style.backgroundColor = 'white';
+		},1000)
+	})
+}
+
